bf5c8635d456040d7583e67419568c68312eebfb
James Devlin <devlin.ja@northeastern.edu>
2020-08-24 20:17:20 -0400

    Created Printing Functions
    Created functions to pretty print success and failure messages, along
    with a means of resetting the coloring once printed.

--- check_site.bash
+++ check_site.bash
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 
+SUCCESS_CODE="\e[32m"
+FAILURE_CODE="\e[31m"
+RESET_CODE="\e[0m"
+
+success() {
+    echo -e "$SUCCESS_CODE""Success: $1"
+}
+
+failure() {
+    echo -e "$FAILURE_CODE""Failure: $1"
+}
+
+reset() {
+    echo -n -e "$RESET_CODE"
+}
+
+success "Hello World!"
+failure "Hello World!"
+reset
 echo "Hello World!"
