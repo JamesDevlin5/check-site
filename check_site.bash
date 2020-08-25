706a1431fde57362ca67df1f63990331fe80a937
James Devlin <devlin.ja@northeastern.edu>
2020-08-24 20:21:58 -0400

    Created `check_site` Function
    Created function to ping a site, a predefined number of times, and
    return whether the ICMP request was successful. Added a test for
    Google's DNS, with an example printing.

--- check_site.bash
+++ check_site.bash
@@ -4,6 +4,8 @@ SUCCESS_CODE="\e[32m"
 FAILURE_CODE="\e[31m"
 RESET_CODE="\e[0m"
 
+NUM_ATTEMPTS=3
+
 success() {
     echo -e "$SUCCESS_CODE""Success: $1"
 }
@@ -16,7 +18,17 @@ reset() {
     echo -n -e "$RESET_CODE"
 }
 
+check_site() {
+    ping -c $NUM_ATTEMPTS "$1" > /dev/null
+    return $?
+}
+
 success "Hello World!"
 failure "Hello World!"
+if check_site 8.8.8.8
+then
+    success "Ping worked!"
+else
+    failure "Ping failed!"
+fi
 reset
-echo "Hello World!"
